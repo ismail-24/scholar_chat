11511ae03d79a839991e081b63de41b239ee8a96
ismail-24 <ismailtarek888@gmail.com>
2023-11-09 16:11:16 +0200

    Create message_model.dart

---
+++ lib/models/message_model.dart
@@ -0,0 +1,11 @@
+import 'package:scholar_chat/constants.dart';
+
+class Message {
+  final String message;
+
+  Message(this.message);
+
+  factory Message.fromJson(jsonData) {
+    return Message(jsonData[kMessage]);
+  }
+}
